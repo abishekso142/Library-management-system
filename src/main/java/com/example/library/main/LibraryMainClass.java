5d33ebd2226302a3b7f6832346bacb95ddf87354
I320035 <abishek.s.o@sap.com>
2019-12-24 20:20:32 +0530

    Add main class

---
+++ src/main/java/com/example/library/main/LibraryMainClass.java
@@ -0,0 +1,16 @@
+package com.example.library.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMainClass
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(LibraryMainClass.class,args);
+
+    }
+
+}
